2bd97aa9b5b3e215ac9c1023e2daa64425331d6b
Jalil Nouri <nouri.6412@gmail.com>
2021-05-18 17:01:19 +0430

    125

--- plugins/ipcommerce/model/post.php
+++ plugins/ipcommerce/model/post.php
@@ -24,7 +24,10 @@ $post_fields = array(
         array(
             "name" => "slug", "title" => "نامک", "size" => 1000, "is_uniq" => true, "attr" => array("col-class" => "col-xs-12 col-sm-6 col-md-6 col-lg-4")
         )
-    )
-    
+    ),
+    new field(array(
+        "name" => "img", "title" => "تصویر", "in_table" => false, "type" => "json", "size" => 2000, "file" => "gallery", "attr" => array("col-class" => "col-xs-12 col-sm-12 col-md-12 col-lg-12")
+    )),
+
 );
 $post_model = new model(array("name" => "post", "title" => "نوشته", "db_model" => true, "icon" => "fa fa-tags", "fields" => $post_fields));
